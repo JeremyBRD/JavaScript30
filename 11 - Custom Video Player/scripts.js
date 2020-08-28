311f7d3cb69524b7fa05c6f06a95075954b7622f
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-08-28 17:14:17 +0200

    ex 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,8 @@
+/* Get our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+
+/* Build the Function */
+
+/* Hook The event listeners */
